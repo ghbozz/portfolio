495789a51c1cd24700786794b580f44ee23a2409
romain sanson <romainsanson.gm@gmail.com>
2018-12-03 07:18:04 +0100

    some js & css on skills

--- javascript/application.js
+++ javascript/application.js
@@ -1,9 +1,11 @@
 let navbar = document.querySelector('.navbar-custom')
 let cards = document.querySelectorAll('.card')
+let skills = document.querySelector('.skills')
 
 window.addEventListener('scroll', event => {
   navbarChange(window.scrollY)
   cardGrow(window.scrollY)
+  skillsUp(window.scrollY)
 })
 
 const navbarChange = function(e) {
@@ -18,11 +20,18 @@ const cardGrow = function(e) {
   if(e > 600) {
     cards.forEach((card) => {
       card.classList.add('card-grow')
-      console.log('io')
     })
   } else {
     cards.forEach((card) => {
       card.classList.remove('card-grow')
     })
+  }
+}
+
+const skillsUp = function(e) {
+  if(e > 1650) {
+    skills.classList.remove('skills-up')
+  } else {
+    skills.classList.add('skills-up')
   }
 }
