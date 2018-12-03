3cfddb0929ab7502bdbe098915c3dbd4f7e7e49b
romain sanson <romainsanson.gm@gmail.com>
2018-12-03 06:39:01 +0100

    some js on cards

--- javascript/application.js
+++ javascript/application.js
@@ -1,7 +1,9 @@
 let navbar = document.querySelector('.navbar-custom')
+let cards = document.querySelectorAll('.card')
 
 window.addEventListener('scroll', event => {
   navbarChange(window.scrollY)
+  cardGrow(window.scrollY)
 })
 
 const navbarChange = function(e) {
@@ -9,5 +11,18 @@ const navbarChange = function(e) {
     navbar.classList.add('navbar-black')
   } else {
     navbar.classList.remove('navbar-black')
+  }
+}
+
+const cardGrow = function(e) {
+  if(e > 600) {
+    cards.forEach((card) => {
+      card.classList.add('card-grow')
+      console.log('io')
+    })
+  } else {
+    cards.forEach((card) => {
+      card.classList.remove('card-grow')
+    })
   }
 }
