8255f2513e43517149ff220048315050a0d605cc
romain sanson <romainsanson.gm@gmail.com>
2018-12-04 08:14:19 +0100

    animation on cards

--- javascript/application.js
+++ javascript/application.js
@@ -1,8 +1,11 @@
 let navbar = document.querySelector('.navbar-custom')
-let cards = document.querySelectorAll('.card')
+let firstCard = document.getElementById('first-card')
+let secondCard = document.getElementById('second-card')
+let thirdCard = document.getElementById('third-card')
 let skills = document.querySelector('.skills')
 let banner = document.querySelector('.banner')
 let bannerContent = document.querySelector('.banner-content')
+let line = document.querySelector('.line')
 
 const btnAbout = document.getElementById('about')
 const about = document.querySelector('.about')
@@ -17,14 +20,26 @@ window.addEventListener('load', event => {
 })
 
 window.addEventListener('scroll', event => {
+  console.log(window.scrollY)
   navbarChange(window.scrollY)
-  cardGrow(window.scrollY)
+  firstCardGrow(window.scrollY)
+  secondCardRise(window.scrollY)
+  thirdCardGrow(window.scrollY)
   skillsUp(window.scrollY)
   bannerSkew(window.scrollY)
+  lineFw(window.scrollY)
 })
 
 const bannerContentOpacity = function(e) {
   bannerContent.classList.add('banner-content-opacity')
+}
+
+const lineFw = (e) => {
+  if (e > 2500) {
+    line.classList.add('line-fw')
+  } else {
+    line.classList.remove('line-fw')
+  }
 }
 
 const bannerSkew = function(e) {
@@ -45,21 +60,33 @@ const navbarChange = function(e) {
   }
 }
 
-const cardGrow = function(e) {
-  if(e > 700) {
-    cards.forEach((card) => {
-      card.classList.add('card-grow')
-    })
+const firstCardGrow = function(e) {
+  if(e > 650) {
+    firstCard.classList.add('card-grow')
   } else {
-    cards.forEach((card) => {
-      card.classList.remove('card-grow')
-    })
+    firstCard.classList.remove('card-grow')
+  }
+}
+
+const secondCardRise = function(e) {
+  if(e > 1100) {
+    secondCard.classList.add('card-rise')
+  } else {
+    secondCard.classList.remove('card-rise')
+  }
+}
+
+const thirdCardGrow = function(e) {
+  if(e > 1560) {
+    thirdCard.classList.add('card-grow')
+  } else {
+    thirdCard.classList.remove('card-grow')
   }
 }
 
 
 const skillsUp = function(e) {
-  if(e > 2200) {
+  if(e > 2100) {
     skills.classList.remove('skills-up')
   } else {
     skills.classList.add('skills-up')
