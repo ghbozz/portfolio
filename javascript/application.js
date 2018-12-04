4d0c22c8a39c86f3312fd1a9ba0f83e2440e6fbc
romain sanson <romainsanson.gm@gmail.com>
2018-12-04 03:02:38 +0100

    tweak

--- javascript/application.js
+++ javascript/application.js
@@ -1,6 +1,8 @@
 let navbar = document.querySelector('.navbar-custom')
 let cards = document.querySelectorAll('.card')
 let skills = document.querySelector('.skills')
+let banner = document.querySelector('.banner')
+let bannerContent = document.querySelector('.banner-content')
 
 const btnAbout = document.getElementById('about')
 const about = document.querySelector('.about')
@@ -14,10 +16,21 @@ window.addEventListener('scroll', event => {
   navbarChange(window.scrollY)
   cardGrow(window.scrollY)
   skillsUp(window.scrollY)
+  bannerSkew(window.scrollY)
 })
 
+const bannerSkew = function(e) {
+  if (e > 100) {
+    banner.classList.add('banner-skew')
+    bannerContent.classList.add('banner-content-skew')
+  } else {
+    banner.classList.remove('banner-skew')
+    bannerContent.classList.remove('banner-content-skew')
+  }
+}
+
 const navbarChange = function(e) {
-  if(e > 650) {
+  if(e > 550) {
     navbar.classList.add('navbar-black')
   } else {
     navbar.classList.remove('navbar-black')
